15b0110c2adee90a84b1132463564db9d3f69a7d
Max <karkinhowo@gmail.com>
2025-08-06 14:47:46 -0300

    table updates

--- Ava3/Cafeteria/analisis.py
+++ Ava3/Cafeteria/analisis.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 
 def main():
     data = pd.read_csv( "result.csv" )
-    data.columns=["RunTime" , "Media Chegada Cliente", "Media Tempo Atendimento","Clientes Atendidos", "Media de Espera", "Media Total" , "Utilizacao" ]
+    data.columns=["RunTime" , "Média Chegada Cliente", "Média Tempo Atendimento","Clientes Atendidos", "Média de Espera", "Média Total" , "Utilização" ]
     data.drop('RunTime', axis=1, inplace=True)
     
 
     
-    data1 = data[data["Media Chegada Cliente"] == 5].groupby( "Media Tempo Atendimento" ).mean().reset_index()
-    data2 = data[data["Media Chegada Cliente"] == 10].groupby( "Media Tempo Atendimento" ).mean().reset_index()
-    data3 = data[data["Media Chegada Cliente"] == 20].groupby( "Media Tempo Atendimento" ).mean().reset_index()
-    data4 = data[data["Media Chegada Cliente"] == 40].groupby( "Media Tempo Atendimento" ).mean().reset_index()
+    data1 = data[data["Média Chegada Cliente"] == 5].groupby( "Média Tempo Atendimento" ).mean().reset_index()
+    data2 = data[data["Média Chegada Cliente"] == 10].groupby( "Média Tempo Atendimento" ).mean().reset_index()
+    data3 = data[data["Média Chegada Cliente"] == 20].groupby( "Média Tempo Atendimento" ).mean().reset_index()
+    data4 = data[data["Média Chegada Cliente"] == 40].groupby( "Média Tempo Atendimento" ).mean().reset_index()
     
     result = pd.concat([data1, data2, data3, data4], axis=0, ignore_index=True)
 
